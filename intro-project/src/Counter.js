1e1c2b6eec6d32cf46dde56dfb5b480ece0d0d3d
Tristan Grovender <46428421+tristangrovender@users.noreply.github.com>
2021-08-02 14:16:59 -0500

    Added Counter component

---
+++ intro-project/src/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = () => {
+  return (
+    <div>
+      <p>This is our counter component</p>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ intro-project/src/Documentation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Documentation() {
+  return (
+    <div>
+      <h1>This is our new component</h1>
+    </div>
+  );
+}
+
+export default Documentation;
